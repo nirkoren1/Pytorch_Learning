4513d95c20a43ed9c74f3f160eb8f3e6c57938c1
nirkoren1 <72827063+nirkoren1@users.noreply.github.com>
2023-08-17 18:46:40 +0300

    final model

--- model.py
+++ model.py
@@ -7,16 +7,19 @@ class Model(nn.Module):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.conv1 = nn.Conv2d(1, 8, 3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.relu = nn.ReLU()
         self.max_pool = nn.MaxPool2d(2, 2)
-        self.drop_out = nn.Dropout(0.1)
-        self.conv2 = nn.Conv2d(8, 1, 3, padding=1)
-        self.fc = nn.Linear(7*7, 10)
+        self.drop_out = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(64*14*14, 256)
+        self.fc2 = nn.Linear(256, 10)
         self.sm = nn.Softmax(dim=1)
         self.flat = nn.Flatten(1, -1)
-        self.layers = [self.conv1, self.relu, self.max_pool, self.drop_out, self.conv2,
-                       self.relu, self.max_pool, self.drop_out, self.flat, self.fc, self.sm]
+        self.layers = [self.conv1, self.relu,
+                       self.conv2, self.relu, self.max_pool,
+                       self.drop_out,
+                       self.flat, self.fc1, self.relu, self.fc2, self.sm]
         self.model = nn.Sequential(*self.layers)
 
     def feed_forward(self, x):
@@ -26,6 +29,6 @@ class Model(nn.Module):
 
 if __name__ == '__main__':
     batch = torch.randn(32, 1, 28, 28)
-    model = Model(32)
+    model = Model()
     ret = model.feed_forward(batch)
     print(ret.shape, ret)
--- training.py
+++ training.py
@@ -6,29 +6,52 @@ import torch.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 
+def show_progress(epoch, step, total_steps, loss, accuracy, width=30, bar_char='█', empty_char='░'):
+    print('\r', end='')
+    progress = ""
+    for i in range(width):
+        progress += bar_char if i < int(step / total_steps * width) else empty_char
+    print(f"epoch:{epoch + 1} [{progress}] {step}/{total_steps} loss: {loss:.4f} accuracy: {accuracy:.4f}", end='')
+    if step >= total_steps - 1:
+        print()
+
 file_name = "./data/mnist_train_small.csv"
 torch.manual_seed(1)
 x, y = preprocessing(file_name)
-batch_size = 32
+batch_size = 64
 model = Model()
-epochs = 200
+epochs = 5
 loss = nn.CrossEntropyLoss()
 lossess = []
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-for i in range(epochs):
-    rand = torch.randint(0, x.shape[0], (batch_size,))
-    batch_x, batch_y = x[rand], y[rand]
-    y_hat = model.feed_forward(batch_x)
-    current_loss = loss(y_hat, batch_y)
-    lossess.append(current_loss.item())
-    current_loss.backward()
-    optimizer.step()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.003, momentum=0.9, weight_decay=0.00)
+model.train()
+for epoch in range(epochs):
+    acc = 0
+    loss_sum = 0
+    for i in range(x.shape[0] // batch_size):
+        optimizer.zero_grad()
+        rand = torch.randint(0, x.shape[0], (batch_size,))
+        batch_x, batch_y = x[rand], y[rand]
+        y_hat = model.feed_forward(batch_x)
+        current_loss = loss(y_hat, batch_y)
+        lossess.append(current_loss.item())
+        loss_sum += current_loss.item()
+        current_loss.backward()
+        optimizer.step()
+        for j in range(batch_size):
+            if torch.argmax(y_hat[j]) == torch.argmax(batch_y[j]):
+                acc += 1
+        # print(loss_sum)
+        show_progress(epoch, i, x.shape[0] // batch_size, current_loss.item(), acc / (batch_size * (i + 1)))
+
+
 file_name_test = "./data/mnist_test.csv"
 x_test, y_test = preprocessing(file_name_test)
 s = 0
+model.eval()
 for i in range(1000):
-    y_hat = model.feed_forward(x_test[i])
-    print(y_hat, y_test[i])
+    y_hat = model.feed_forward(x_test[i].reshape(1, 1, 28, 28))
+    # print(y_hat, y_test[i])
     if torch.argmax(y_hat) == torch.argmax(y_test[i]):
         s += 1
 print(f"precision loss {s/1000}")
@@ -38,6 +61,8 @@ def moving_average(a, n):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
+
+
 
 
 plt.plot(moving_average(lossess, 10))
